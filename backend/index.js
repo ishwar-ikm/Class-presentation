6846ffd74b3896a8db3b25b2e0ae4f895573a50b
Ishwar Mishra <ikmishra2002@gmail.com>
2024-10-13 22:14:57 +0530

    prepared for development

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import { connectToDB } from "./db/connectToDB.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -9,11 +10,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  })
+}
 
 app.listen(PORT, () => {
   connectToDB();
